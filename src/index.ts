7ad0f00d34f93d44bbe50e7edbea037e31882c93
s-hirano-ist <solucky0725@icloud.com>
2023-11-16 12:18:32 +0900

    add codes

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { generateUrlFile, importUrlFile } from "./common/util";
+import { JSDOM } from "jsdom";
+
+const IMPORT_FILE_NAME = "./original/bookmarks_latest.html";
+const OUTPUT_DIR = "./output/";
+
+(async () => {
+  console.log("Converting html bookmark links to .url format files");
+
+  const data = await importUrlFile(IMPORT_FILE_NAME);
+  const aTagList = new JSDOM(data).window.document.getElementsByTagName("A");
+
+  for (let i = 0; i < aTagList.length; i++) {
+    const title = aTagList.item(i)?.textContent;
+    const href = aTagList.item(i)?.getAttribute("href");
+
+    var marks = ["\\", "/", ":", "*", "?", "a", "<", ">", "|"];
+
+    let sanitizedTitle = title ?? "";
+    for (let i = 0; i < marks.length; i++) {
+      sanitizedTitle = sanitizedTitle.replaceAll(marks[i], "_");
+    }
+
+    if (!href) console.error(`DOM error on ${title}`);
+
+    console.log(title, href);
+    await generateUrlFile(OUTPUT_DIR, `${sanitizedTitle}.url`, href ?? "");
+  }
+})();
